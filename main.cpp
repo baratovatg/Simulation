5114138a89c7a6ca59d6f6c7f4c82f26529c7edc
EgorZhuchkov <egorzhuchkov2013@yandex.ru>
2019-05-30 23:57:09 +0300

    Added some screenshots

--- main.cpp
+++ main.cpp
@@ -14,14 +14,11 @@ int main()
     Field simField(size);
     simField.genTrees(10);
     simField.genRabbits(numOfRabbits);
-//    simField.draw();
     while(true) {
-        printf("\e[1;1H\e[2J");
+        printf("\e[1;1H\e[2J"); //Вместо очистки экрана
         simField.draw();
         simField.changeState();
         sleep(2);
     }
 }
-
-//░▒▓10
 
